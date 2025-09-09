b528ae300ba7e205c5d08249744c3fdf1eed6215
DharShan07vk <dharshanvk07@gmail.com>
2025-09-09 22:48:33 +0530

    feat: add Firebase configuration and initialization

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAQQotjDy6v9IXgGNUtTYegbGOh3zvV7No",
+  authDomain: "stemforsociety-9b0d6.firebaseapp.com",
+  projectId: "stemforsociety-9b0d6",
+  storageBucket: "stemforsociety-9b0d6.firebasestorage.app",
+  messagingSenderId: "575044446880",
+  appId: "1:575044446880:web:bec19f82eafd39ded577c5",
+  measurementId: "G-WJ28FKKWQS"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
